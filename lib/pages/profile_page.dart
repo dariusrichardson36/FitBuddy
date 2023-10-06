9b99bf24bf3cd3d8937ae1ef9debef8288a18b4c
Zacg713 <136399468+Zacg713@users.noreply.github.com>
2023-10-05 21:46:25 -0500

    Added post to page
    Added post to the profile page.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -8,12 +8,13 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(left: 20, top: 20),
+        padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
+                // User profile picture in top left
                 Container(
                   width: 75.0,
                   height: 75.0,
@@ -116,7 +117,7 @@ class ProfilePage extends StatelessWidget {
 
             Row(
               children: [
-                SizedBox(width: 280),
+                SizedBox(width: 270),
 
                 IconButton(
                   onPressed: () {},
@@ -126,10 +127,258 @@ class ProfilePage extends StatelessWidget {
                   )
                 ),
               ],
-            )
+            ),
             
+            Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+
+            Row(
+              children: [
+                Container(
+                  width: 45.0,
+                  height: 45.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://pbs.twimg.com/profile_images/1650839170653335552/WgtT2-ut_400x400.jpg'), // Replace with your image URL
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Zachary',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Text(
+                      '8h ago',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 13
+                      ),
+                    )
+                  ],
+                ), 
+              ]
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Activity',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 5),
+
+                    Text(
+                      'Leg Press',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Text(
+                      'Bicep Curl',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(width: 15),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                     '|',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 52)
+                  ],
+                ),
+
+                SizedBox(width: 15),
+  
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Sets',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 5),
+
+                    Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(width: 15),
+  
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                     '|',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 52)
+                  ],
+                ),
+
+                SizedBox(width: 15),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Reps',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 5),
+
+                    Text(
+                      '12',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Text(
+                      '12',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(width: 15),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                     '|',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 52)
+                  ],
+                ),
+
+                SizedBox(width: 15),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Weight',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 5),
+
+                    Text(
+                      '125',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Text(
+                      '125',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+
+
           ],
-          
         ),
       ),
     );
