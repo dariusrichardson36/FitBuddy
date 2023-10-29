c8a7dd15e3369eb8fb0b8f5c6c0902a4f015d52e
Zacg713 <136399468+Zacg713@users.noreply.github.com>
2023-10-29 16:39:32 -0500

    added user name and image to the drawer

--- lib/pages/drawer.dart
+++ lib/pages/drawer.dart
@@ -1,6 +1,7 @@
-
 import 'package:fit_buddy/constants/color_constants.dart';
 import 'package:fit_buddy/constants/route_constants.dart';
+import 'package:fit_buddy/models/user.dart';
+import 'package:fit_buddy/services/firestore.dart';
 import 'package:fit_buddy/theme/theme_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,135 +12,174 @@ import '../services/auth.dart';
 class DrawerPage extends StatelessWidget {
   const DrawerPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20, top: 10),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Row(
-            children: [
-              Container(
-                width: 75.0,
-                height: 75.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        'https://pbs.twimg.com/profile_images/1650839170653335552/WgtT2-ut_400x400.jpg'), // Replace with your image URL
-                    fit: BoxFit.cover,
-                  ),
+  Widget ui(BuildContext context, User user) {
+    String? name;
+    String? image;
+
+    // sets name variable and gives default value if NULL.
+    if (user.name != null) {
+      name = user.name;
+    } else {
+      name = "Name Unknown";
+    }
+
+    // sets image variable and gives default value if NULL.
+    if (user.image != null) {
+      image = user.image;
+    } else {
+      image = "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg";
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 10),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Row(
+          children: [
+            Container(
+              width: 75.0,
+              height: 75.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage(image!), // Replace with your image URL
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(width: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Zachary@Zac.Graham',
-                    style: TextStyle(
-                        color: FitBuddyColorConstants.lOnPrimary,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name!,
+                  style: TextStyle(
+                      color: FitBuddyColorConstants.lOnPrimary,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 35),
+                Text(
+                  '215 Friends',
+                  style: TextStyle(
+                    color: FitBuddyColorConstants.lOnSecondary,
+                    fontSize: 10,
                   ),
-                  SizedBox(height: 35),
-                  Text(
-                    '215 Friends',
-                    style: TextStyle(
-                      color: FitBuddyColorConstants.lOnSecondary,
-                      fontSize: 10,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 50),
-          Row(
-            children: [
-              Icon(Icons.person, color: FitBuddyColorConstants.lOnPrimary, size: 35),
-              SizedBox(width: 15),
-              ElevatedButton(
-                  onPressed: () {
-                    context.goNamed(FitBuddyRouterConstants.profilePage);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent,
-                  ),
-                  child: Text(
-                    'Profile',
-                    style: TextStyle(color: FitBuddyColorConstants.lOnPrimary, fontSize: 18),
-                  )),
-            ],
-          ),
-          SizedBox(height: 15),
-          Row(
-            children: [
-              Icon(Icons.settings, color: FitBuddyColorConstants.lOnPrimary, size: 35),
-              SizedBox(width: 15),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent,
-                  ),
-                  child: Text(
-                    'Settings',
-                    style: TextStyle(color: FitBuddyColorConstants.lOnPrimary, fontSize: 18),
-                  )),
-            ],
-          ),
-          SizedBox(height: 15),
-          Row(
-            children: [
-              Icon(Icons.edit, color: FitBuddyColorConstants.lOnPrimary, size: 35),
-              SizedBox(width: 15),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent,
-                  ),
-                  child: Text(
-                    'Log Workout',
-                    style: TextStyle(color: FitBuddyColorConstants.lOnPrimary, fontSize: 18),
-                  )),
-            ],
-          ),
-          SizedBox(height: 15),
-          Row(
-            children: [
-              Icon(Icons.search, color: Colors.black, size: 35),
-              SizedBox(width: 15),
-              ElevatedButton(
-                  onPressed: () {
-                    context.goNamed(FitBuddyRouterConstants.searchPage);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent,
-                  ),
-                  child: Text(
-                    'Search',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  )),
-            ],
-          ),
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                  onPressed: () => Auth().signOutUser(),
-                  icon: Icon(Icons.logout_rounded)),
-              IconButton(
-                  onPressed: () => ThemeManager().toggleTheme(),
-                  icon: Icon(Icons.sunny))
-            ],
-          ),
-        ]),
-      ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(height: 50),
+        Row(
+          children: [
+            Icon(Icons.person,
+                color: FitBuddyColorConstants.lOnPrimary, size: 35),
+            SizedBox(width: 15),
+            ElevatedButton(
+                onPressed: () {
+                  context.goNamed(FitBuddyRouterConstants.profilePage);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                      color: FitBuddyColorConstants.lOnPrimary, fontSize: 18),
+                )),
+          ],
+        ),
+        SizedBox(height: 15),
+        Row(
+          children: [
+            Icon(Icons.settings,
+                color: FitBuddyColorConstants.lOnPrimary, size: 35),
+            SizedBox(width: 15),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                child: Text(
+                  'Settings',
+                  style: TextStyle(
+                      color: FitBuddyColorConstants.lOnPrimary, fontSize: 18),
+                )),
+          ],
+        ),
+        SizedBox(height: 15),
+        Row(
+          children: [
+            Icon(Icons.edit,
+                color: FitBuddyColorConstants.lOnPrimary, size: 35),
+            SizedBox(width: 15),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                child: Text(
+                  'Log Workout',
+                  style: TextStyle(
+                      color: FitBuddyColorConstants.lOnPrimary, fontSize: 18),
+                )),
+          ],
+        ),
+        SizedBox(height: 15),
+        Row(
+          children: [
+            Icon(Icons.search, color: Colors.black, size: 35),
+            SizedBox(width: 15),
+            ElevatedButton(
+                onPressed: () {
+                  context.goNamed(FitBuddyRouterConstants.searchPage);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                child: Text(
+                  'Search',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                )),
+          ],
+        ),
+        Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+                onPressed: () => Auth().signOutUser(),
+                icon: Icon(Icons.logout_rounded)),
+            IconButton(
+                onPressed: () => ThemeManager().toggleTheme(),
+                icon: Icon(Icons.sunny))
+          ],
+        ),
+      ]),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<User> test = FireStore.FireStore().getUserData();
+    
+    return SafeArea(
+        child: FutureBuilder(
+            future: test,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                User? user = snapshot.data;
+                if (user != null) {
+                  return ui(context, user);
+                }
+                return Text('User not Found');
+              } else {
+                return CircularProgressIndicator();
+              }
+            }));
   }
 }
