1804ff04e7066336fe1ff47a875de0dd6d1535f0
Robin Monseré <robin.monsere@student.howest.be>
2023-10-31 00:02:00 +0100

    dropdown button menu items with text

--- lib/components/FitBuddyVisibilitySelector.dart
+++ lib/components/FitBuddyVisibilitySelector.dart
@@ -1,11 +1,9 @@
-import 'package:cool_dropdown/cool_dropdown.dart';
-import 'package:cool_dropdown/models/cool_dropdown_item.dart';
 import 'package:fit_buddy/constants/color_constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 
-class FitBuddyVisibilitySelector extends StatefulWidget {
+class FitBuddyVisibilitySelector extends StatelessWidget {
   final onChanged;
   final String value;
 
@@ -17,51 +15,64 @@ class FitBuddyVisibilitySelector extends StatefulWidget {
 
 
   @override
-  State<FitBuddyVisibilitySelector> createState() => _FitBuddyVisibilitySelectorState();
-}
-
-class _FitBuddyVisibilitySelectorState extends State<FitBuddyVisibilitySelector> {
-  List<CoolDropdownItem<String>> visibilityDropdownItems = [];
-  final visibilityDropdownController = DropdownController();
-  final List<String> options = [
-    'Public',
-    'Private',
-  ];
-
-  @override
-  void initState() {
-    for (var i = 0; i < options.length; i++) {
-      visibilityDropdownItems.add(
-        CoolDropdownItem<String>(
-            label: options[i],
-            icon: SizedBox(
-              height: 25,
-              width: 25,
-              child: Icon(
-                options[i] == 'Private' ? Icons.visibility_off : Icons.visibility,
-                color: Colors.white, // Set your desired icon color here
-              )
-            ),
-            value: options[i]),
-      );
-    }
-    super.initState();
-  }
-  @override
   Widget build(BuildContext context) {
     return SizedBox (
       width: 60,
       height: 40,
       child: DropdownButtonHideUnderline(
-        child: CoolDropdown<String>(
-          dropdownList: visibilityDropdownItems,
-          controller: visibilityDropdownController,
-
-          onChange: (selected) {
-            widget.onChanged(selected);
-            visibilityDropdownController.close();
-          },
-          defaultItem: visibilityDropdownItems.first,
+        child: DropdownButton2<String>(
+          iconStyleData: const IconStyleData(
+            icon: Icon(Icons.keyboard_arrow_down_rounded, color: Colors.white,),
+            openMenuIcon: Icon(Icons.keyboard_arrow_up_rounded, color: Colors.white,),
+            iconSize: 24,
+          ),
+          buttonStyleData: ButtonStyleData(
+            height: 40,
+            width: 50,
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            decoration: BoxDecoration(
+              color: FitBuddyColorConstants.lAccent,
+              borderRadius: const BorderRadius.horizontal(right: Radius.circular(20.0)),
+            ),
+          ),
+          menuItemStyleData: const MenuItemStyleData(
+            height: 40,
+          ),
+          dropdownStyleData: DropdownStyleData(
+            offset: const Offset(-60, -2),
+            isOverButton: false,
+            width: 120,
+            padding: EdgeInsets.zero,
+            decoration: BoxDecoration(
+              color: FitBuddyColorConstants.lAccent,
+              borderRadius: BorderRadius.circular(10)
+            )
+          ),
+          onChanged: onChanged,
+          value: value,
+          items: <String>['Private', 'Public'].map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Row(
+                children: [
+                  Icon(
+                    value == 'Private' ? Icons.visibility_off : Icons.visibility,
+                    color: Colors.white, // Set your desired icon color here
+                  ),
+                  const SizedBox(width: 10),
+                  Text(value, style: TextStyle(color: FitBuddyColorConstants.lPrimary),)
+                ],
+              ),
+            );
+          }).toList(),
+          selectedItemBuilder: (BuildContext context) {
+            return <String>['Private', 'Public'].map<Widget>((String value) {
+              return Icon(
+                value == 'Private' ? Icons.visibility_off : Icons.visibility,
+                color: Colors.white, // Set your desired icon color here
+              );
+            }).toList();
+          }
         ),
       ),
     );
